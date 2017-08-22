7bb87eaaf42147a5a88f638f13ed8729abb1bf2d
abhirammv <amadenur@ucsc.edu>
2017-08-22 15:23:13 -0700

    Commit 2 InsSort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
+    int arr[] = {31, 41, 59, 26, 41, 58};
+    int size = sizeof(arr)/ sizeof(arr[0]);
+    int key = 0;
+    int j = 0;
+
+    //Insertion Sort
+    for(int i = 1; i < size; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1] = arr[j];
+            j--;
+
+        }
+        arr[j+1] = key;
+
+    }
+
+    for(int l = 0; l < size; l++)
+        cout<<arr[l]<<endl;
+
     return 0;
 }
 
