67ead395a00bfd70c666bf77991e560f4696bc71
abhirammv <amadenur@ucsc.edu>
2017-08-22 16:40:25 -0700

    Insertion Sort Linked Lists

--- InsSortLL.cpp
+++ InsSortLL.cpp
@@ -30,15 +30,60 @@ void PrintList(Node* head)
     }
 }
 
+void insert(Node** head_sorted, Node* newNode)
+{
+    if(*head_sorted == NULL || newNode->data <= (*head_sorted)->data)
+    {
+        newNode->next = *head_sorted;
+        *head_sorted = newNode;
+
+    }
+
+    else
+    {
+        Node* start = *head_sorted;
+        while(start->next!=NULL && start->next->data <= newNode->data)
+            start = start->next;
+
+        newNode->next = start->next;
+        start->next = newNode;
+    }
+
+}
+
+
+void InsertionSort(Node** head)
+{
+    Node* sorted = NULL;
+    Node* start = *head;
+
+    while(start!=NULL)
+    {
+        Node* next = start->next;
+        insert(&sorted, start);
+        start = next;
+
+    }
+
+    *head = sorted;
+}
+
+
 int main()
 {
     Node* l1 = NULL;
+
     push(&l1, 5);
     push(&l1, 20);
     push(&l1, 4);
     push(&l1, 3);
     push(&l1, 30);
+
     PrintList(l1);
+
+    InsertionSort(&l1);
+    PrintList(l1);
+
     return 0;
 
 }
