19a0a160468023efa8b43a052ae2035ad629c83c
abhirammv <amadenur@ucsc.edu>
2017-08-22 15:50:14 -0700

    commit 2

--- InsSortLL.cpp
+++ InsSortLL.cpp
@@ -7,6 +7,16 @@ struct Node
     struct Node* next;
 
 };
+//Function to append elements to the beginning of the linked list
+Node* push(Node* head, int data)
+{
+    Node* newNode = new Node;
+    newNode->data = data;
+
+    newNode->next = head;
+    return newNode;
+
+}
 
 int main()
 {
